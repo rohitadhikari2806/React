fdef5a0016a86341b6b164a3ed19d9d33268f263
rohitadhikari2806 <166013554+rohitadhikari2806@users.noreply.github.com>
2025-05-09 22:35:57 +0530

    Update Login.jsx

--- Blog Mern/frontend/src/components/Login.jsx
+++ Blog Mern/frontend/src/components/Login.jsx
@@ -1,43 +1,4 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import { useNavigate } from 'react-router-dom';
 
-// function Login() {
-//   const [form, setForm] = useState({
-//     email: '',
-//     password: ''
-//   });
-
-//   const navigate = useNavigate();
-
-//   const handleChange = (e) => {
-//     setForm({ ...form, [e.target.name]: e.target.value });
-//   };
-
-//   const handleLogin = async () => {
-//     try {
-//       await axios.post('http://localhost:5000/api/users/login', form, {
-//         withCredentials: true
-//       });
-//       alert('Login Successful');
-//       navigate('/dashboard');
-//     } catch (error) {
-//       console.error('Login error:', error);
-//       alert(error.response?.data?.message || 'Invalid Credentials');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h2>Login</h2>
-//       <input type="email" name="email" placeholder="Email" onChange={handleChange} /><br />
-//       <input type="password" name="password" placeholder="Password" onChange={handleChange} /><br />
-//       <button onClick={handleLogin}>Login</button>
-//     </div>
-//   );
-// }
-
-// export default Login
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
